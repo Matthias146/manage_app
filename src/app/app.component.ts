85527dfd8fa24617a868634a04c7b98674532e50
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2025-06-08 23:40:55 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+  ],
 })
-export class AppComponent {
-  title = 'manage-app';
-}
+export class AppComponent {}
